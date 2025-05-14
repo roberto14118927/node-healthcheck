3ccb865ff271e44700b9238b3ef57299b0892bba
roberto <robertoguzman776@gmail.com>
2025-05-14 13:01:24 -0600

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/healthcheck', (_req, res) => {
-  res.status(200).json({ status: 'staging', timestamp: new Date().toISOString() });
+  res.status(200).json({ status: 'staging', timestamp: new Date().toISOString(), test: 'success'  });
 });
 
 app.get('/names', (_req, res) => {
